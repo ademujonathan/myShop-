e51fbba40341c73a5dfe6dd1261ee24277bba4c4
unknown <ademujonathan@hotmail.com>
2020-04-25 18:50:54 +0100

    initial commit

---
+++ products/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from products.models import Product
+
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Joe\PycharmProjects\myShop\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
